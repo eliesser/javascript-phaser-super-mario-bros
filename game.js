619be0cee7c802a999ed3baf05c4a55a8f83b2a6
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-20 13:29:46 +0200

    first commit

---
+++ game.js
@@ -0,0 +1,28 @@
+/* global Phaser */
+
+const config = {
+  type: Phaser.AUTO,
+  width: 256,
+  height: 244,
+  backgroundColor: '#fff',
+  parent: 'game',
+  scene: {
+    preload, // se ejecuta para cargar recursos
+    create, // se ejecuta cuando el juego comienza
+    update // se ejecuta en cada frame
+  }
+}
+
+new Phaser.Game(config)
+
+function preload() {
+  console.log('preload')
+}
+
+function create() {
+  console.log('create')
+}
+
+function update() {
+
+}
